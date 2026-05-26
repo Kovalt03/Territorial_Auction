165f3f6d7aaa5689fdc1fe65f5eef2bbe2b05539
Kovalt03 <kalswp2003@naver.com>
2026-05-27 08:54:37 +0900

    [FEAT] ContinentRepository·DTO·Service에 display 필드 반영

--- backend/src/main/java/com/territorial/auction/domain/map/repository/ContinentRepository.java
+++ backend/src/main/java/com/territorial/auction/domain/map/repository/ContinentRepository.java
@@ -3,4 +3,6 @@ package com.territorial.auction.domain.map.repository;
 import com.territorial.auction.domain.map.entity.Continent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContinentRepository extends JpaRepository<Continent, Long> {}
+public interface ContinentRepository extends JpaRepository<Continent, Long> {
+    boolean existsByDisplayNameIsNull();
+}
